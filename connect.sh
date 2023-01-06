3bae66f0cd0a60cebd9915bc514c8a165577d83f
Maximiliano Zapata <maximiliano.zapata@intraway.com>
2023-01-06 13:16:42 -0300

    Add extra parameters

--- connect.sh
+++ connect.sh
@@ -4,6 +4,6 @@
 
 echo "nameserver 8.8.8.8"  > /etc/resolv.conf
 echo "nameserver 8.8.4.4" >> /etc/resolv.conf
-/bin/sh -c "echo $VPN_PASS | openconnect $VPN_URL --user=$VPN_USER --passwd-on-stdin" 2> /dev/null
+/bin/sh -c "echo $VPN_PASS | openconnect $VPN_URL $VPN_EXTRA_PARAMS --user=$VPN_USER --passwd-on-stdin" 2> /dev/null
 
 echo "Murió la flor"

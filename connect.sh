002a594cdc222c1d1fac7b163c5b1e1f317d3bbb
Maximiliano Zapata <maximiliano.zapata@intraway.com>
2023-01-06 14:18:11 -0300

    add debugging lines

--- connect.sh
+++ connect.sh
@@ -6,6 +6,7 @@ date
 
 echo "nameserver 8.8.8.8"  > /etc/resolv.conf
 echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+echo 'Executing: openconnect $VPN_URL $VPN_EXTRA_PARAMS --user=$VPN_USER --pass=$VPN_PASS'
 /bin/sh -c "echo $VPN_PASS | openconnect $VPN_URL $VPN_EXTRA_PARAMS --user=$VPN_USER --passwd-on-stdin" 2> /dev/null
 
 echo "Murió la flor"
